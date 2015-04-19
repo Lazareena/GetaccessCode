cc1fbc42acf10e882ff0b8559a9abccd4e5eb2ca
Reena <lthaveet@macalester.edu>
2015-04-19 15:38:42 -0500

    Get access code
    Tries to get access code using json object and url

---
+++ app/src/main/java/com/example/reena/getaccesscode/Person.java
@@ -0,0 +1,27 @@
+package com.example.reena.getaccesscode;
+
+/**
+ * Created by Reena on 4/19/15.
+ */
+public class Person {
+    private String name;
+    private String password;
+    private String granType;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
